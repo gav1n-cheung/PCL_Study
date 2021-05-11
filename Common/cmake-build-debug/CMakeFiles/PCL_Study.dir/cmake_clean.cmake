eaa61ebaa7424563e3d181ec2fcf39f41da0338a
gav1n-cheung <gavinbr@outlook.com>
2021-05-11 17:24:34 +0800

    第四次提交

---
+++ Common/cmake-build-debug/CMakeFiles/PCL_Study.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PCL_Study.dir/NoteOfDocuments/MatrixTrans.cpp.o"
+  "CMakeFiles/PCL_Study.dir/NoteOfDocuments/NoteOfUsage.cpp.o"
+  "CMakeFiles/PCL_Study.dir/main.cpp.o"
+  "PCL_Study"
+  "PCL_Study.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PCL_Study.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
