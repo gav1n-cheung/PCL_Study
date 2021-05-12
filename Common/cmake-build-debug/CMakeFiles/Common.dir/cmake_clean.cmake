24a7c75774d78248e3540e5c675c15f0d08ebdbb
gav1n-cheung <gavinbr@outlook.com>
2021-05-12 08:55:45 +0800

    冲突解决

--- Common/cmake-build-debug/CMakeFiles/Common.dir/cmake_clean.cmake
+++ Common/cmake-build-debug/CMakeFiles/Common.dir/cmake_clean.cmake
@@ -1,9 +1,9 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/Common.dir/NoteOfDocuments/MatrixTrans.cpp.o"
-  "CMakeFiles/Common.dir/NoteOfDocuments/NoteOfUsage.cpp.o"
   "CMakeFiles/Common.dir/main.cpp.o"
-  "Common"
+  "CMakeFiles/Common.dir/NoteOfDocuments/NoteOfUsage.cpp.o"
+  "CMakeFiles/Common.dir/NoteOfDocuments/MatrixTrans.cpp.o"
   "Common.pdb"
+  "Common"
 )
 
 # Per-language clean rules from dependency scanning.
